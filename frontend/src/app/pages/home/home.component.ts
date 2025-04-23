0118e8dfe6540ef83b06c7471dae81f65303ca03
tabessi-adam <tabessidadam2@gmail.com>
2025-04-23 18:02:44 +0200

    a

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -4,11 +4,10 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { WhyChooseUsComponent } from './why-choose-us/why-choose-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, FooterComponent, HeroSectionComponent, HowItWorksComponent, WhyChooseUsComponent, ContactUsComponent],
+  imports: [NavbarComponent, FooterComponent, HeroSectionComponent, HowItWorksComponent, WhyChooseUsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
