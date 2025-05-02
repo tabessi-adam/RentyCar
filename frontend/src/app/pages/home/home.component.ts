d363025cb5d3c40f5089f949ed3b625eeaa72a7d
tabessi-adam <tabessidadam2@gmail.com>
2025-05-02 15:14:00 +0200

    Add BrandsComponent to HomePage: Introduce a new BrandsComponent to showcase brand logos, update home.component.html to include the new component, and enhance home.component.ts with the necessary imports. Add corresponding HTML, SCSS, and spec files for the BrandsComponent, along with brand images for display.

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -4,10 +4,18 @@ import { FooterComponent } from '../../shared/footer/footer.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { WhyChooseUsComponent } from './why-choose-us/why-choose-us.component';
+import { BrandsComponent } from './brands/brands.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, FooterComponent, HeroSectionComponent, HowItWorksComponent, WhyChooseUsComponent],
+  imports: [
+    NavbarComponent, 
+    FooterComponent, 
+    HeroSectionComponent, 
+    HowItWorksComponent, 
+    WhyChooseUsComponent,
+    BrandsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
