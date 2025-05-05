f862beaeb741b7999f82345c6b3e2e9f9179c576
tabessi-adam <tabessidadam2@gmail.com>
2025-05-05 02:21:41 +0200

    Implement translation support in reservations component: Update reservations.component.html to use ngx-translate for dynamic title and subtitle rendering. Modify reservations.component.ts to include TranslateModule in imports. Add new translation keys for reservations in English and French JSON files for improved localization.

--- frontend/src/app/client/reservations/reservations.component.ts
+++ frontend/src/app/client/reservations/reservations.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { ReservationsListComponent } from './reservations-list/reservations-list.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { ReservationsListComponent } from './reservations-list/reservations-list.component';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-reservations',
   standalone: true,
-  imports: [CommonModule, ReservationsListComponent, NavbarComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TranslateModule,
+    ReservationsListComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './reservations.component.html',
   styleUrl: './reservations.component.scss'
 })
